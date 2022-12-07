9bc8305f37b5bbcdd4715db4e4779f77d5563383
Maurus Graf <maurus.graf@ergon.ch>
2022-12-07 08:13:10 +0000

    add duration, role and state enums

---
+++ coworking-at-zurich-park-side/src/main/java/ch/zli/m223/lb2/model/Duration.java
@@ -0,0 +1,6 @@
+package ch.zli.m223.lb2.model;
+
+public enum Duration {
+    MORNING,
+    AFTERNOON
+}
---
+++ coworking-at-zurich-park-side/src/main/java/ch/zli/m223/lb2/model/State.java
@@ -0,0 +1,8 @@
+package ch.zli.m223.lb2.model;
+
+public enum State {
+    PENDING,
+    ACCEPTED,
+    DECLINED,
+    CANCELLED
+}
