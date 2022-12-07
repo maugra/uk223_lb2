e55a3547b6e0e3f3142c566647e1570142db66a5
Maurus Graf <maurus.graf@ergon.ch>
2022-12-07 08:08:25 +0000

    implement Credential model

--- coworking-at-zurich-park-side/src/main/java/ch/zli/m223/lb2/model/Credential.java
+++ coworking-at-zurich-park-side/src/main/java/ch/zli/m223/lb2/model/Credential.java
@@ -1,5 +1,30 @@
+//übernommen von punchclock Lösungsvorschlag
 package ch.zli.m223.lb2.model;
 
+import javax.validation.constraints.NotBlank;
+
 public class Credential {
-    
+
+  @NotBlank
+  private String email;
+
+  @NotBlank
+  private String password;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
 }
