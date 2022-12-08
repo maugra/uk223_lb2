46a26435f267be4569d79edbbd7650e55b215f6d
Maurus Graf <maurus.graf@ergon.ch>
2022-12-08 15:31:58 +0100

    add Transactional annotation
    Signed-off-by: Maurus Graf <maurus.graf@ergon.ch>

--- src/main/java/ch/zli/m223/lb2/service/BookingService.java
+++ src/main/java/ch/zli/m223/lb2/service/BookingService.java
@@ -1,43 +1,47 @@
 package ch.zli.m223.lb2.service;
 
-import java.util.List;
+import ch.zli.m223.lb2.model.Booking;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.persistence.EntityManager;
-
 import javax.inject.Inject;
-
-import ch.zli.m223.lb2.model.Booking;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @ApplicationScoped
 public class BookingService {
-    @Inject
-    EntityManager entityManager;
-    @Inject
-    ApplicationUserService applicationUserService;
+	@Inject
+	EntityManager entityManager;
+	@Inject
+	ApplicationUserService applicationUserService;
 
-    public Booking createBooking(Booking booking) {
-        return entityManager.merge(booking);
-    }
+	@Transactional
+	public Booking createBooking(Booking booking) {
+		return entityManager.merge(booking);
+	}
 
-    public List<Booking> findAll() {
-        var query = entityManager.createQuery("FROM Booking", Booking.class);
-        return query.getResultList();    
-    }
+	@Transactional
+	public List<Booking> findAll() {
+		var query = entityManager.createQuery("FROM Booking", Booking.class);
+		return query.getResultList();
+	}
 
-    public Booking updateBooking(Long id, Booking booking) {
-        booking.setId(id);
-        return entityManager.merge(booking);
-    }
+	@Transactional
+	public Booking updateBooking(Long id, Booking booking) {
+		booking.setId(id);
+		return entityManager.merge(booking);
+	}
 
-    public void deleteBooking(Long id) {
-        var booking = entityManager.find(Booking.class, id);
-        entityManager.remove(booking);
-    }
+	@Transactional
+	public void deleteBooking(Long id) {
+		var booking = entityManager.find(Booking.class, id);
+		entityManager.remove(booking);
+	}
 
-    public List<Booking> getBookingsOfUser(Long userId) {
-        var query = entityManager.createQuery("SELECT b FROM booking b WHERE b.applicationuser_id = :id", Booking.class);
-        query.setParameter("id", userId);
-        return query.getResultList();
-    } 
+	@Transactional
+	public List<Booking> getBookingsOfUser(Long userId) {
+		var query = entityManager.createQuery("SELECT b FROM booking b WHERE b.applicationuser_id = :id", Booking.class);
+		query.setParameter("id", userId);
+		return query.getResultList();
+	}
 }
