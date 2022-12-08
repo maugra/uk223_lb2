6574d1859afca1082cc0eb0042af2dc722322857
Maurus Graf <maurus.graf@ergon.ch>
2022-12-08 07:47:43 +0000

    implement jwt authentication

--- src/main/java/ch/zli/m223/lb2/service/SessionService.java
+++ src/main/java/ch/zli/m223/lb2/service/SessionService.java
@@ -1,13 +1,45 @@
 package ch.zli.m223.lb2.service;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Response;
 
+import com.oracle.svm.core.annotate.Inject;
+
+import ch.zli.m223.lb2.model.ApplicationUser;
 import ch.zli.m223.lb2.model.Credential;
+import io.smallrye.jwt.build.Jwt;
 
 @ApplicationScoped
 public class SessionService {
-    public Response authenticate(Credential credential){
-        return Response.status(Response.Status.OK).build();
+
+  @Inject
+  ApplicationUserService applicationUserService;
+  // übernommen aus punchclock
+  public Response authenticate(Credential credential) {
+    Optional<ApplicationUser> principal = applicationUserService.findByEmail(credential.getEmail());
+
+    try {
+      if (principal.isPresent() && principal.get().getPassword().equals(credential.getPassword())) {
+        String token = Jwt
+            .issuer("https://zli.example.com/")
+            .upn(credential.getEmail())
+            .groups(new HashSet<>(Arrays.asList("Member", "Admin")))
+            .expiresIn(Duration.ofHours(24))
+            .sign();
+        return Response
+            .ok(principal.get())
+            .header("Authorization", "Bearer " + token)
+            .build();
+      }
+    } catch (Exception e) {
+      System.err.println("Couldn't validate password.");
     }
+
+    return Response.status(Response.Status.FORBIDDEN).build();
+  }
 }
